d72f4faeb8c0e9a25b6181cc59d172f61fb6c8f2
joachimdalen <joachimdalen@users.noreply.github.com>
2023-01-10 18:10:28 +0000

    Bump formula to v0.1.2

--- Formula/bicepdocs.rb
+++ Formula/bicepdocs.rb
@@ -1,9 +1,9 @@
 class Bicepdocs < Formula
   desc "A tool to build documentation for Bicep modules"
   homepage "https://github.com/joachimdalen/bicepdocs"
-  version "0.1.1"
-  url "https://github.com/joachimdalen/bicepdocs/releases/download/v0.1.1/bicepdocs-osx-x64"
-  sha256 "3e8f333f5637d6c4a50eb35538e49da2ae55996759750305016dd6a686d10687"
+  version "0.1.2"
+  url "https://github.com/joachimdalen/bicepdocs/releases/download/v0.1.2/bicepdocs-osx-x64"
+  sha256 "b7ab6321f4acd5299b872a18bd936a767fd8848a7b76151973d5902ed4bb3dd2"
 
   license "MIT"
 
